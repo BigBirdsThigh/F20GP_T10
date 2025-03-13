812833c447b5b2383a47463fe482e95f827c191d
im2013 <114154589+im2013@users.noreply.github.com>
2025-03-13 12:45:50 +0000

    added player movement and the camera movement

---
+++ f20gpDungeonGame/Assets/Scripts/Player_Camera.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class Player_Camera : MonoBehaviour
+{
+
+    public float rotationSpeed = 5f; //rotation speed
+    public float rotX = 0f; //x axis from the mouse input
+    public float rotY = 0f; //y axis from the mouse input
+
+    private GameObject player;
+    
+    public Vector3 TP_Offset = Vector3.zero; //set this in the inspector tab- it is an vector where we can modify where the camera's position will be
+    public float fov = -4f; //how close the third person camera is to the player
+    public float focusSpeed = 5f; //how fast the camera will move
+
+    public float maxLookupAngle = 80f; //how far ontop of the player we can see (e.g. how close to the player's head/top)
+    public float minLookupAngle = -25f; //how below the player we can see (e.g. how close to the floor)
+
+    public float maxFOV = -1f;
+    public float minFOV = -4.5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player"); //get the player object from the scene
+    }
+
+    //late update to prevent glitchy camera- as movement is done in fixed update
+    void LateUpdate()
+    {
+        ThirdPersonCamera();
+        AdjustFOV();
+    }
+
+    void ThirdPersonCamera()
+    {
+        //get the mouse movement to move around the camera
+        float mX = Input.GetAxis("Mouse X") * rotationSpeed;
+        float mY = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        rotY += mX; //left and right
+        rotX -= mY; //up and down
+
+        rotX = Mathf.Clamp(rotX, minLookupAngle, maxLookupAngle); //limit the x rotation, should be modifiable in inspector
+        Quaternion cameraRotation = Quaternion.Euler(rotX, rotY, 0f); //rotate the camera position along the y and x axis
+
+        Vector3 targetOffset = TP_Offset;
+        Vector3 cameraRotationFinal = cameraRotation * targetOffset; //add the offset to the initial camera position to get the final position
+
+        this.transform.position = Vector3.Slerp(this.transform.position, player.transform.position + cameraRotationFinal, Time.deltaTime * focusSpeed);
+        this.transform.LookAt(player.transform.position); //look at player
+
+    }
+
+    void AdjustFOV()
+    {
+        fov = Mathf.Clamp(fov, minFOV, maxFOV); //minimum and maximum distances for the fov
+        TP_Offset = new Vector3(TP_Offset.x, TP_Offset.y, fov); //update the camera offset position
+    }
+}
---
+++ f20gpDungeonGame/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,148 @@
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    private Rigidbody rb; //the player rigidbody
+
+    //movement speed variables
+    public float currentMovementSpeed = 0f;
+    public float walkSpeed = 10f;
+    public float runSpeed = 20f;
+    public float rotationSpeed = 10f;
+
+    //camera movement variables
+    public bool isMoving = false;
+    private float stopRotTimer = 0f;
+    public float stopRotDur = 0.5f; //time in secs after stopping before camera+player rotation stops
+    public float turnRot = 25f;
+    public float fixRot = -90f; //fixer for camera movement, fixes bugs
+
+    //jumping and falling variables
+    public float raycastMaxDistance = 2f;
+    public bool isGrounded = true;
+    public float jumpHeight = 50f;
+    public float fallMult;
+
+    private GameObject camera; //getting the player's camera from the scene
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //get the rigidbody of the player
+        rb = this.GetComponent<Rigidbody>();
+        camera = GameObject.FindGameObjectWithTag("MainCamera"); //get camera from scene
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementChecker(); //checks 'isMoving' either True or False
+        PlayerSpeed(); //increases player speed if LEFT SHIFT held
+        CheckGrounded(); //check if player is on the ground for jumping
+        Jump(); //jumping for the player, done pressing spacebar
+    }
+
+    void FixedUpdate() //fixed update for handling in game physics with rigidbody movement
+    {
+        BasicMovement();
+        RotationMovement();
+    }
+
+    void BasicMovement()
+    {
+        Vector3 moveDirection = transform.right * Input.GetAxis("Vertical") + transform.forward * -Input.GetAxis("Horizontal"); //reverse horizontal movement
+        rb.linearVelocity = new Vector3(moveDirection.x * currentMovementSpeed, rb.linearVelocity.y, moveDirection.z * currentMovementSpeed);
+    }
+
+    void RotationMovement()
+    {
+        //if camera is still- dont rotate the player
+        //if camera moving- rotate the player to face where the camera + player movement is going
+
+        //get the rotation of the camera
+        float camYRot = camera.transform.eulerAngles.y;
+        float camXRot = camera.transform.eulerAngles.x;
+
+        //if moving, rotate the camera to always be behind the player
+        if(isMoving)
+        {
+            Quaternion goalRotation = Quaternion.Euler(0f, camYRot + fixRot, 0f); //fix rotation ensures camera is always behind the player
+            float turnAmountAngle = Input.GetAxis("Horizontal") * turnRot; //turn the player slightly when moving left or right
+            transform.rotation = Quaternion.Slerp(transform.rotation, goalRotation, rotationSpeed * Time.deltaTime);
+            stopRotTimer = stopRotDur; //reset timer when moving
+        }
+        else if (stopRotTimer > 0) //if stopping moving, stop camera rotating with the player
+        {
+            Quaternion goalRotation = Quaternion.Euler(0f, camYRot + fixRot, 0f); //fix rotation ensures camera is always behind the player
+            float turnAmountAngle = Input.GetAxis("Horizontal") * turnRot; //turn the player slightly when moving left or right
+            goalRotation *= Quaternion.Euler(0f, turnAmountAngle, 0f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, goalRotation, rotationSpeed * Time.deltaTime);
+            stopRotTimer -= Time.deltaTime; //reset timer when moving
+        }
+        else { //if movement stopped completely
+            rb.angularVelocity = Vector3.zero; //completely stop all angular movement to prevent bugs
+        }
+    }
+
+    void MovementChecker()
+    {
+        //method to check whether or not the player is moving
+        if(Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
+
+    void PlayerSpeed()
+    {
+        //if get key down, make player speed set to the running speed, vice versa
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            //make running speed
+            currentMovementSpeed = runSpeed;
+        }
+        else 
+        {
+            //make walking speed
+            currentMovementSpeed = walkSpeed;
+        }
+    }
+
+    void CheckGrounded()
+    {
+        //shoot a raycast down from the bottom of the player
+        RaycastHit hit;
+        Vector3 origin = this.transform.position;
+        Vector3 direction = Vector3.down;
+
+        if(Physics.Raycast(origin, direction, out hit, raycastMaxDistance)) //should prob change this to height of character + some to get bottom of avatar
+        {
+            if(hit.collider.CompareTag("Ground"))
+            {
+                isGrounded = true;
+            }
+        }
+        else //jumping/falling
+        {
+            isGrounded = false;
+        }
+    }
+
+    void Jump()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, Mathf.Sqrt(2 * jumpHeight * fallMult), rb.linearVelocity.z);
+        }
+        if(!isGrounded)
+        {
+            //if falling, add downwards force
+            rb.linearVelocity += Vector3.down * (fallMult * Time.deltaTime);
+        }
+    }
+}
