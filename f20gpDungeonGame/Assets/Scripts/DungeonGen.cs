26c722478f61befb90344d2895dd805b5627450c
adam_stuart <adam.stuart729@gmail.com>
2025-03-20 12:54:07 +0000

    Updated stuff

--- f20gpDungeonGame/Assets/Scripts/DungeonGen.cs
+++ f20gpDungeonGame/Assets/Scripts/DungeonGen.cs
@@ -1,32 +1,59 @@
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class DungeonGen : MonoBehaviour
 {
-    static private int room_count = 2; // hardcoded uh oh
-    private Dictionary<string, int> oneXone = new Dictionary<string, int>();
-    private Dictionary<string, int> oneXtwo = new Dictionary<string, int>();
-    private Dictionary<string, int>[] possible_rooms = new Dictionary<string, int>[room_count];
+    // Grid variables
+    public int grid_width = 5;
+    public int grid_height = 5;
+    private string[,] grid;
     
+    // Room variables
+    static private int room_count = 2; // hardcoded uh oh
+    private Room Room1x1 = new Room("Room1x1", 1, 1);
+    private Room Room1x2 = new Room("Room1x2", 1, 2);
+    private Room[] possible_rooms = new Room[room_count];
+    
+    
+    // Other variables
+    // go here
+
     void Start()
     {
-        // oops more hardcoding, it's unavoidable
-        oneXone.Add("width", 1); oneXone.Add("height", 1); possible_rooms[0] = oneXone;
-        oneXtwo.Add("width", 1); oneXtwo.Add("height", 2); possible_rooms[1] = oneXtwo;
+        // Fill list of possible rooms, hardcoding unavoidable
+        possible_rooms[0] = Room1x1;
+        possible_rooms[1] = Room1x2;
+
+        // create empty grid
+        grid = new string[grid_height, grid_width];
+        //PrintGrid(grid);
+        int empty_count = grid_height * grid_width;
+
+        // other variable for genning grid
+        LinkedList<(int,int)> adjacent_rooms= new LinkedList<(int,int)>();
+        int[] ROTATIONS = {0,90};
+
+        // Add rooms until there are no more empty spaces
+        while (empty_count > 0) {
+            // Chose a room and location for the room
+            Room room_choice  = possible_rooms[Random.Range(0, possible_rooms.Length)];
+            int origin_x = Random.Range(0, grid_width);
+            int origin_y = Random.Range(0, grid_height);
+            
+            
+            
+            
+            
+            empty_count -= 1;
+        }
 
 
+        
+
+        
     
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     }
 
@@ -34,5 +61,21 @@ public class DungeonGen : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public static void PrintGrid(string[,] grid)
+    {
+        string grid_text = "";
+
+        for (int y = 0; y < grid.GetLength(0); y++) 
+        { 
+            for (int x = 0; x < grid.GetLength(1); x++) 
+            { 
+                grid_text += grid[y,x] + " "; 
+            } 
+            grid_text += "\n";
+        }  
+
+        Debug.Log(grid_text);
     }
 }
