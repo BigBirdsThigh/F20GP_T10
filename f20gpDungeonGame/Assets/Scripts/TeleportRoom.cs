77915c8dd2d50b56f27e03e79ac1dcfba1730a8d
Adam Stuart <91395037+AdamStuart12@users.noreply.github.com>
2025-04-02 19:54:38 +0100

    Added teleport room

--- f20gpDungeonGame/Assets/Scripts/TeleportRoom.cs
+++ f20gpDungeonGame/Assets/Scripts/TeleportRoom.cs
@@ -6,27 +6,40 @@ public class TeleportRoom : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    private GameObject[,] pads = new GameObject[6,6];
+    private (GameObject,(int,int))[,] pads = new (GameObject,(int,int))[6,6]; // contains the telepad and the destination of that pad
+    private (int,int)[,] connections = {{(3,1), (0,2), (0,1), (2,2), (4,5), (1,3)},
+                                        {(5,4), (4,2), (2,5), (0,5), (5,0), (3,0)},
+                                        {(5,2), (4,0), (0,3), (5,1), (3,2), (1,2)},
+                                        {(1,5), (0,0), (2,4), (4,3), (5,3), (4,4)},
+                                        {(2,1), (5,5), (1,1), (3,3), (3,5), (0,4)},
+                                        {(1,4), (2,3), (2,0), (3,4), (1,0), (4,1)}};
+
     public GameObject telepad_obj;
+    private Dictionary<GameObject,(int,int)> lookup = new Dictionary<GameObject,(int,int)>(); // used to find the telepad in the list pads without looping through every time
+    private GameObject player;
+    private bool teleportable = true;
+    private GameObject targetPad;
+
+
     void Start()
     {
-        //Vector3 origin = this.gameObject.transform.position;
+        AddTelepads();
+
+
+        
+    }
+
+    private void AddTelepads() {
         for (int y = 0; y < 6; y++) {
             for (int x = 0; x < 6; x++) {
                 float xn = (-16.5f + (x*7));
                 float zn = (-16.5f + (y*7));
-                pads[y,x] = Instantiate(telepad_obj);
-                pads[y,x].transform.parent = transform;
-                pads[y,x].transform.localPosition = new Vector3(xn,0,zn);
-                pads[y,x].transform.rotation = Quaternion.Euler(-90,0,0);
-
-                /*
-                GameObject gFloor = Instantiate(Floor);
-                gFloor.transform.parent = transform;
-                gFloor.transform.localPosition = new Vector3(x,0,z) * fSpacing;
-                gFloor.transform.rotation = Quaternion.Euler(-90,0,0);
-                gFloor.name = "Floor_" + z + x ;
-                */
+                GameObject obj = Instantiate(telepad_obj);
+                obj.transform.parent = transform;
+                obj.transform.localPosition = new Vector3(xn,0,zn);
+                obj.transform.rotation = Quaternion.Euler(-90,0,0);
+                pads[y,x] = (obj,connections[y,x]);
+                lookup.Add(obj,(y,x));
             }
         }
     }
@@ -34,6 +47,26 @@ public class TeleportRoom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player == null) {
+            player = GameObject.FindGameObjectWithTag("Player");
+        } else {
+            foreach ((GameObject,(int,int)) pad in pads) {
+                if (teleportable) { 
+                    if (Vector3.Distance(player.transform.position, pad.Item1.transform.position) < 2) {
+                        targetPad = pads[pad.Item2.Item1,pad.Item2.Item2].Item1;
+                        Vector3 targetLocation = targetPad.transform.position;
+                        targetLocation.x -= 1; targetLocation.z -= 1; targetLocation.y = 0.5f;
+                        player.transform.position = targetLocation;
+                        teleportable = false;
+                        //Debug.Log("Teleported to " + pad.Item2.Item1 + ", " + pad.Item2.Item2);
+                    }
+                } else {
+                    if (Vector3.Distance(player.transform.position, targetPad.transform.position) > 3){
+                        teleportable = true;
+                        //Debug.Log("reset");
+                    }
+                }
+            }
+        }
     }
 }
